bbc653033a1de257caf63afa8840cc5d03082448
maria <147421482+mariaR5@users.noreply.github.com>
2026-08-04 00:22:51 +0530

    Time: 40 ms (78.68%) | Memory: 93.9 MB (91.19%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -4,16 +4,14 @@ public:
         sort(intervals.begin(), intervals.end(), [] (auto &a, auto &b) {
             return a[1] < b[1];
         });
-        
+
         int end = intervals[0][1];
         int i = 1;
         int count = 0;
 
         while (i < intervals.size()) {
-            if (end > intervals[i][0]) {
+            if (end > intervals[i][0])
                 count++;
-                end = min(end, intervals[i][1]);
-            }
             else end = intervals[i][1];
             i++;
         }
