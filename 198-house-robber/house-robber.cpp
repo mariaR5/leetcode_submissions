5df4caee37d7d0a23a5e9ca6afac4356a737dbb0
maria <147421482+mariaR5@users.noreply.github.com>
2026-09-05 21:57:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.09%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -13,7 +13,21 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return f(n - 1, nums, dp);
+        vector<int> dp(n, 0);
+        // return f(n - 1, nums, dp);
+
+        dp[0] = nums[0];
+
+        if (n > 1)
+            dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int pick = nums[i] + dp[i-2];
+            int nonPick = 0 + dp[i-1];
+
+            dp[i] = max(pick, nonPick);
+        }
+
+        return dp[n-1];
     }
 };
