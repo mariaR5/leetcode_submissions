46258b0404f6420580ccee6e1c6719d136da55b3
maria <147421482+mariaR5@users.noreply.github.com>
2026-09-06 15:34:08 +0530

    Time: 2 ms (35.38%) | Memory: 15.3 MB (89.76%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -19,26 +19,21 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<int> dp(n, INT_MAX);
         
-        dp[0][0] = grid[0][0];
+        dp[0] = grid[0][0];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) continue;
 
-                int up = INT_MAX;
-                int left = INT_MAX;
+                int up = dp[j];
+                int left = (j > 0) ? dp[j-1] : INT_MAX;
 
-                if (i > 0)                                                                  
-                    up = grid[i][j] + dp[i-1][j];
-                if (j > 0)
-                    left = grid[i][j] + dp[i][j-1];
-                
-                dp[i][j] = min(up, left);    
+                dp[j] = grid[i][j] + min(up, left);
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
